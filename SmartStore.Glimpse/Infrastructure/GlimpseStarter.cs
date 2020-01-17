d4a4927de3355886e392c48fbd6f33ceb24dce7f
Murat Cakir <muratc@smartstore.de>
2020-01-18 00:20:43 +0100

    IPostApplicationStart > runs once AFTER Application_Start with the very first request and very early in the request lifecycle (renamed IStartupTask to IApplicationStart accordingly).

--- SmartStore.Glimpse/Infrastructure/GlimpseStarter.cs
+++ SmartStore.Glimpse/Infrastructure/GlimpseStarter.cs
@@ -3,33 +3,28 @@
 //using Glimpse.EF;
 using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 using SmartStore.Core.Infrastructure;
-using SmartStore.Core.Plugins;
 using G = Glimpse;
 
 namespace SmartStore.Glimpse.Infrastructure
 {
-	public class GlimpseStarter : IPreApplicationStart, IStartupTask
+	public class GlimpseStarter : IPreApplicationStart, IApplicationStart
     {
+        // Ensure this runs before EF initializes
+        int IApplicationStart.Order => -2000;
 
-        public void Start()
+        void IPreApplicationStart.Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(GlimpseHttpModule));
 
-			//G.Ado.Initialize.Start();
-			//G.EF.Initialize.Start();
+            //G.Ado.Initialize.Start();
+            //G.EF.Initialize.Start();
         }
 
-        public void Execute()
+        void IApplicationStart.Start()
         {
-			// register Glimpse trace listener
-			var listener = new G.Core.TraceListener();
-			Trace.Listeners.Add(listener);
-        }
-
-        public int Order
-        {
-            // ensure this runs before EF initializes
-            get { return -2000; }
+            // register Glimpse trace listener
+            var listener = new G.Core.TraceListener();
+            Trace.Listeners.Add(listener);
         }
     }
 }
